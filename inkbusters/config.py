66ab751b9ec82f8b44e123c45dcaf7788ad5ce7e
halsing <adrian.zieba2@gmail.com>
2020-01-18 19:48:38 +0100

    new configuration for ubuntu

--- inkbusters/config.py
+++ inkbusters/config.py
@@ -1,11 +1,14 @@
 import os
+import json
 
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.environ.get("SECRET_KEY") or config.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "inkbusters.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = os.environ.get("DEBUG_MODE")
+    DEBUG = os.environ.get("DEBUG_MODE") or config.get("DEBUG_MODE")
